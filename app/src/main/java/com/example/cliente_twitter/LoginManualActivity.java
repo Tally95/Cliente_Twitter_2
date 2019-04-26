852715fefa15b2386341b277063032202c83bd03
betod <betodm1994@hotmail.com>
2019-04-25 23:48:57 -0600

    Second commit

---
+++ app/src/main/java/com/example/cliente_twitter/LoginManualActivity.java
@@ -0,0 +1,22 @@
+package com.example.cliente_twitter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginManualActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_manual);
+    }
+
+    public void goHomepage(View view) {
+        String username = "Manual Login Simulation, there is no connection with the Twitter API";
+        Intent intent  = new Intent(LoginManualActivity.this, Homepage.class);
+        intent.putExtra("username", username);
+        startActivity(intent);
+    }
+}
